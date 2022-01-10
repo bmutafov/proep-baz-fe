cc2b077ebcb3e474d4904d55a71f3e84360bcb68
unknown <402846@student.fontys.nl>
2022-01-10 16:48:54 +0100

    update iframe dimensions

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,11 +14,10 @@ const Dashboard: React.FC = () => {
     >
       <iframe
         title="PowerBI - live_data - Numerical Data"
-        width="1400"
-        height="600"
         src="https://app.powerbi.com/reportEmbed?reportId=5881575d-ac80-4c58-b3b8-0410eb93c16f&autoAuth=true&ctid=c66b6765-b794-4a2b-84ed-845b341c086a&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1mLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
         frameBorder={0}
         allowFullScreen
+        style={{ width: "100%", height: "100%" }}
       ></iframe>
     </Box>
   );
